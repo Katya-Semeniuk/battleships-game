79fd730471f18b616cb131e1acaf674e5739c743
Katya-Semeniuk <katyasemeniuk@gmail.com>
2024-04-06 11:57:29 +0200

    created new game function

--- run.py
+++ run.py
@@ -36,6 +36,56 @@ class Board:
              self.ships.append((x, y))
              if self.type == 'player':
                  self.board [x][y] = '@'
+
+
+
+
+def random_point(size):
+    """Helper function to return a random integer beetween 0 and size"""
+    return randint(0, size -1)
+
+def valid_coordinates(x, y, board):
+    print("Valid coordinate function")
+
+def populate_board(board):
+    print("Populate board function")
+
+def make_guess(board):
+    print("make guess function")
+
+def play_game(computer_board, player_board):
+    print("Play game function")
+
+def new_gane():
+    """Starts a new game. Sets the board size and number of ships, resets the 
+    scores and initializes the boards"""
+
+    size = 5
+    num_ships = 4
+    scores["computer"] = 0
+    scores["player"] = 0
+    print('-' * 35)
+    print("Welcome to ULTIMATE BUTTLESHIPS!")
+    print(f"Board Size: {size}. Number of ships: {num_ships}")
+    print("Top lesft corner is row: 0, col:0")
+    print('-' * 35)
+    player_name = input("Enter your name...\n")
+    print('-' * 35)
+
+    computer_board = Board(size, num_ships, "Computer", type="computer")
+    player_board = Board(size, num_ships, player_name, type="player")
+
+    for _ in range(num_ships):
+        populate_board(player_board)
+        populate_board(computer_board)
+
+    play_game(computer_board, player_board)
+
+
+
+
+ new_gane()
+
          
 
 
